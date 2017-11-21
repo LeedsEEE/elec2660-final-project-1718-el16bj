21aad90cfd5a84a364cb5bc195ab6a5f0c2bf446
Ben Johnson [el16bj] <el16bj@eentpc249.leeds.ac.uk>
2017-11-21 12:39:27 +0000

    Initial Commit

---
+++ Mobile Applications Project 2017/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mobile Applications Project 2017
+//
+//  Created by Ben Johnson [el16bj] on 21/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Mobile Applications Project 2017/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mobile Applications Project 2017
+//
+//  Created by Ben Johnson [el16bj] on 21/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
