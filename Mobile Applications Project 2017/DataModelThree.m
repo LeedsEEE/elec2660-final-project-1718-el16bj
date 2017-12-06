6b54d660aa25eebd18614e7ab1c9d3e10c2e49bd
Ben Johnson [el16bj] <el16bj@eentpc253.leeds.ac.uk>
2017-12-06 18:03:45 +0000

    6/12/17
    Finished GUI of all View Controllers.

--- Mobile Applications Project 2017/DataModelThree.m
+++ Mobile Applications Project 2017/DataModelThree.m
@@ -18,31 +18,31 @@
         self.cafearray = [NSMutableArray array];
         Cafe *baineswingcafe = [[Cafe alloc] init];
         baineswingcafe.cafename = @"The Baines Wing Cafe";
-        baineswingcafe.cafelocation = @"The Baines Wing Cafe is located in The Baines Wing Building ground floor";
+        baineswingcafe.cafelocation = @"Located in The Baines Wing Building ground floor.";
         baineswingcafe.openingtimes = @"Opening Times: MON-FRI: 8:00AM-3:00PM";
         
         self.cafearray = [NSMutableArray array];
         Cafe *fuse = [[Cafe alloc] init];
         fuse.cafename = @"FUSE";
-        fuse.cafelocation = @"The FUSE Cafe is located in The School of Chemical & Porcess Enginerring";
+        fuse.cafelocation = @"Located in The School of Chemical & Porcess Engineering.";
         fuse.openingtimes = @"Opening Times: MON-FRI: 8:00AM-3:30PM";
         
         self.cafearray = [NSMutableArray array];
         Cafe *parkinsoncourtcafe = [[Cafe alloc] init];
         parkinsoncourtcafe.cafename = @"The Parkinson Court Cafe";
-        parkinsoncourtcafe.cafelocation = @"The Parkinson Court Cafe is located in the Parkinson Building on the ground floor";
+        parkinsoncourtcafe.cafelocation = @"Located in the Parkinson Building on the ground floor.";
         parkinsoncourtcafe.openingtimes = @"Opening Times: MON-FRI: 8:30AM-4:00PM";
         
         self.cafearray = [NSMutableArray array];
         Cafe *editroom = [[Cafe alloc] init];
         editroom.cafename = @"The Edit Room";
-        editroom.cafelocation = @"The Edit Room is located in The Edward Boyle Library ground floor";
+        editroom.cafelocation = @"Located in The Edward Boyle Library ground floor.";
         editroom.openingtimes = @"Opening Times: MON-FRI: 8:00AM-6:00PM";
         
         self.cafearray = [NSMutableArray array];
         Cafe *businessschool = [[Cafe alloc] init];
         businessschool.cafename = @"The Business School Cafe";
-        businessschool.cafelocation = @"The Business School Cafe is located in the Business School, on the Western Campus, on the ground floor";
+        businessschool.cafelocation = @"Located in the Business School, on the Western Campus, on the ground floor.";
         businessschool.openingtimes = @"Opening Times: MON-FRI: 8:00AM-5:00PM";
         
         
